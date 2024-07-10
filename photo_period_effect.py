2be5a8e54d5be8a3cd9e8120176a61073236a951
TJJ <113013408+Tanjiaojiao1@users.noreply.github.com>
2024-07-10 10:12:45 +0800

    Update photo_period_effect.py

--- photo_period_effect.py
+++ photo_period_effect.py
@@ -4,8 +4,8 @@ from pylab import *
 def photoeffect_yin(DL, mu, zeta, ep):
     '''
     DL: Actual daylength
-    mu=-15.46, zeta=2.06, ep=2.48
-    After standardization processing, 'photoeffect_yin' represents the impact of the photoperiod on development rate,
+    mu, zeta, and ep are the model parameters.
+    After standardization processing, 'photoeffect_yin' represents the impact of the DL on development rate,
     with values ranging from 0 to 1.
     '''
     def yin_photo(DL, mu, zeta, ep):
@@ -16,15 +16,15 @@ def photoeffect_yin(DL, mu, zeta, ep):
 
 def photoeffect_wofost(DL,Dc,Do):
     '''
-    Dc: the critical day length. Dc=16
-    Do: the Optimum day length. Do=12.5
+    Dc: the critical day length.
+    Do: the Optimum day length.
     '''
     return(min(max(0, ((DL-Dc)/(Do-Dc))), 1))
 
 
 def photoeffect_oryza2000(DL, Dc, PPSE):
     '''
-    PPSE: a light-sensitive parameter. PPSE=0.2
+    PPSE: a light-sensitive parameter.
     '''
     if DL < Dc:
         PPFAC = 1.
@@ -34,14 +34,11 @@ def photoeffect_oryza2000(DL, Dc, PPSE):
     return PPFAC
 
 
-def Test_photoeffect():
-    '''
-    To test whether the functions are correct
-    '''
-    plot(range(0, 24), [photoeffect_wofost(DL=DLm) for DLm in np.linspace(0, 24, 24)])
-    show()
-def CERES_Rice(psr:float=100,DLc:float=12.5,DL:float=10):
-    return (1+psr/136*(DL-DLc))**-1
-if __name__ == '__main__':
-    Test_photoeffect()
+def CERES_Rice(psr,Do,DL):
+    if DL < Do:
+        PPFAC = 1.
+    else:
+        PPFAC = (1+psr/136*(DL-Do))**-1
+    return PPFAC
+
 
